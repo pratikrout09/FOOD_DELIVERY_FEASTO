f42b8139478bd05bd3da03f1129e29ac38baa45e
Pratik Rout <pratikrout844@gmail.com>
2026-01-11 20:21:58 +0530

    Update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://Feasto:18192021@cluster0.wanruwg.mongodb.net/FOOD_DELIVERY').then(()=>console.log("DB Connected!"));
+    //await mongoose.connect('mongodb+srv://Feasto:18192021@cluster0.wanruwg.mongodb.net/FOOD_DELIVERY').then(()=>console.log("DB Connected!"));
+    await mongoose.connect(`${process.env.MONGODB_URI}/FOOD_DELIVERY`).then(()=>console.log("DB Connected!"));
 }
